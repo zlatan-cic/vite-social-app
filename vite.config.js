1e843c3f530b8e8a5d9d72e9e1e12353b8cd0391
zlatan-cic <smajicz999@gmail.com>
2023-10-15 14:45:50 +0200

    Initial init

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vite-social-app",
+  base: "/vite-social",
   plugins: [react()],
 })
